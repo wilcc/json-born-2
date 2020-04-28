4b999d38658ad287b95b2b839977d427d69db450
Wilson Chen <wilson.chen@codeimmersives.com>
2020-04-27 23:52:03 -0400

    no idea

--- front-end/get-users.js
+++ front-end/get-users.js
@@ -7,3 +7,46 @@ const getUsers = () => {
   xhr.open('GET', 'http://localhost:3000/users');
   xhr.send();
 }
+
+const getUser = (num) => {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('loadend', (event) => {
+    console.log(event.target.response);
+  })
+  xhr.open('GET', `http://localhost:3000/users/${num}`);
+  xhr.send();
+}
+const getFriends = (num) =>{
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('loadend', (event) => {
+    const objText =event.target.response;
+    const obj = JSON.parse(objText)
+    // const friend =obj.friends
+    console.log(obj.friends)
+  })
+
+  xhr.open('GET', `http://localhost:3000/users/${num}`);
+  xhr.send();
+
+}
+
+const postUser = (parameter) =>{
+  const {name, age, eyeColor} = parameter;
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('loadend', (event) => {
+    const objText = event.target.response;
+    const obj = JSON.parse(objText)
+    const newUser = {
+      name,
+      age,
+      eyeColor,
+      index: objText.length,
+    }
+    obj.push(newUser);
+    const updatedUsers = JSON.stringify(obj, null, 2);
+
+  })
+  xhr.open('POST', `http://localhost:3000/users`);
+  xhr.send();
+
+}
--- front-end/main.js
+++ front-end/main.js
@@ -1,1 +1,5 @@
-getUsers();
+// getUsers();
+// getUser(7);
+// getFriends(7);
+postUser('Whatever', 22,'red');
+// updateUser(4,'name','Updated!')
